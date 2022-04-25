bbe192edd0161ff3338f194d4b06603f3839e04c
lexluga <lexvillia@gmail.com>
2022-04-25 17:18:52 +0300

    Edit Workflows to Run Docker Builds

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,5 @@
-import { screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
 
-test('renders learn react link', () => {
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
- 
+it('renders without crashing',() => {});
